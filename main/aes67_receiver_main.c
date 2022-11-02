3aa62c803560896406813bfa1346bf33c8b2a591
Takeshi Matsuya <tmatsuya@macchan-mbp15.local>
2022-11-03 01:34:20 +0900

    support detect PCM mode by RTP payload size (L16/24 1/5msec)

--- main/aes67_receiver_main.c
+++ main/aes67_receiver_main.c
@@ -145,56 +145,19 @@ static void mcast_example_task(void *pvParameters)
     static int seq_no_before = -1;
     unsigned char recvbuf[2000];
     //char raddr_name[32] = { 0 };
-    int seq_no, seq_no_diff, len, err;
+    int seq_no, seq_no_diff, len, old_len, err;
     struct sockaddr_storage raddr; // Large enough for IPv4
     socklen_t socklen = sizeof(raddr);
     i2s_chan_handle_t tx_handle;
     size_t bytes_written;
-    int pcm_msec;		// 1 or 5 msec interval
+    int pcm_msec = 1;		// 1 or 5 msec interval
     //int rtp_payload_size;	// 204 (L16 1ms) or 300 (L24 1ms) or 972 (L16 5ms) or 1452 (L24 5ms)
-    int pcm_byte_per_frame;	// 6 (L24) or 4 (L16)
+    int pcm_byte_per_frame = 8;	// 8 (L24) or 4 (L16)
 
-
-#ifdef TO_L16			// L24 to L16 convert
-    pcm_byte_per_frame = 4;
-    pcm_msec = 5;
-#else				// no convert
-    pcm_byte_per_frame = 8;
-    pcm_msec = 5;
-#endif
     //rtp_payload_size = pcm_byte_per_frame * 48 * pcm_msec;
 
 
-    i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
-    //i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
-    i2s_new_channel(&chan_cfg, &tx_handle, NULL);
-
-    i2s_std_config_t std_cfg = {
-        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
-#ifdef TO_L16			// L24 to L16 convert
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
-#else
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
-#endif
-    //    .slot_cfg = I2S_STD_PHILIP_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
-        .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,
-            .bclk = GPIO_NUM_26,
-            .ws = GPIO_NUM_22,
-            .dout = GPIO_NUM_25,
-            .din = I2S_GPIO_UNUSED,
-            .invert_flags = {
-                .mclk_inv = false,
-                .bclk_inv = false,
-                .ws_inv = false,
-            },
-        },
-    };
-    /* Initialize the channel */
-    i2s_channel_init_std_mode(tx_handle, &std_cfg);
-
-    /* Before write data, start the tx channel first */
-    i2s_channel_enable(tx_handle);
+    old_len = -1;
 
     while (1) {
 
@@ -221,10 +184,80 @@ static void mcast_example_task(void *pvParameters)
         // Loop waiting for UDP received, and sending UDP packets if we don't
         // see any.
         err = 1;
+
+        len = recvfrom(sock, recvbuf, sizeof(recvbuf)-1, 0, (struct sockaddr *)&raddr, &socklen);
+        if ( len != old_len ) {
+            switch (len) {
+                case 204:  pcm_byte_per_frame = 4; pcm_msec = 1;  // L16 1ms
+                    break;
+                case 300:  pcm_byte_per_frame = 8; pcm_msec = 1;  // L24 1ms
+                    break;
+                case 972:  pcm_byte_per_frame = 4; pcm_msec = 5;  // L16 5ms
+                    break;
+                case 1452: pcm_byte_per_frame = 8; pcm_msec = 5;  // L24 5ms
+                    break;
+            }
+
+            i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+            //i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
+            i2s_new_channel(&chan_cfg, &tx_handle, NULL);
+
+
+            i2s_std_config_t std16_cfg = {
+                .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
+                .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
+    //            .slot_cfg = I2S_STD_PHILIP_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
+                .gpio_cfg = {
+                    .mclk = I2S_GPIO_UNUSED,
+                    .bclk = GPIO_NUM_26,
+                    .ws = GPIO_NUM_22,
+                    .dout = GPIO_NUM_25,
+                    .din = I2S_GPIO_UNUSED,
+                    .invert_flags = {
+                        .mclk_inv = false,
+                        .bclk_inv = false,
+                        .ws_inv = false,
+                    },
+                },
+            };
+
+            i2s_std_config_t std32_cfg = {
+                .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
+                .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+    //            .slot_cfg = I2S_STD_PHILIP_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
+                .gpio_cfg = {
+                    .mclk = I2S_GPIO_UNUSED,
+                    .bclk = GPIO_NUM_26,
+                    .ws = GPIO_NUM_22,
+                    .dout = GPIO_NUM_25,
+                    .din = I2S_GPIO_UNUSED,
+                    .invert_flags = {
+                        .mclk_inv = false,
+                        .bclk_inv = false,
+                        .ws_inv = false,
+                    },
+                },
+            };
+
+            /* Initialize the channel */
+            if (pcm_byte_per_frame == 4) {
+                i2s_channel_init_std_mode(tx_handle, &std16_cfg);
+            } else {
+                i2s_channel_init_std_mode(tx_handle, &std32_cfg);
+            }
+
+            /* Before write data, start the tx channel first */
+            i2s_channel_enable(tx_handle);
+
+            seq_no_before = -1;
+            old_len = len;
+        }
+    
         while (err > 0) {
-            len = recvfrom(sock, recvbuf, sizeof(recvbuf)-1, 0,
-                               (struct sockaddr *)&raddr, &socklen);
+            len = recvfrom(sock, recvbuf, sizeof(recvbuf)-1, 0, (struct sockaddr *)&raddr, &socklen);
             if (len > 0) {
+                if (len != old_len)
+                    break;
                 //seq_no = ntohs(*(unsigned short *)(recvbuf+2));
                 seq_no =(recvbuf[2] << 8) | (recvbuf[3]);
                 seq_no_diff = seq_no - seq_no_before;
@@ -237,21 +270,21 @@ static void mcast_example_task(void *pvParameters)
                 }
                 seq_no_before = seq_no;
                 // swap byte order
-#ifdef TO_L16			// L24 to L16 convert
-                for (i=0; i<pcm_byte_per_frame*48*pcm_msec; i+=(pcm_byte_per_frame>>1)) {
-                    tmp1          = recvbuf[12+i];
-                    recvbuf[12+i] = recvbuf[13+i];
-                    recvbuf[13+i] = tmp1;
+                if (pcm_byte_per_frame == 8) {
+                    for (i=(48*pcm_msec*2-1); i>=0; --i) {
+                        tmp1            = recvbuf[14+i*3];
+                        recvbuf[15+i*4] = recvbuf[12+i*3];
+                        recvbuf[14+i*4] = recvbuf[13+i*3];
+                        recvbuf[13+i*4] = tmp1;
+                        recvbuf[12+i*4] = 0;
+                    }
+                } else if (pcm_byte_per_frame == 4) {
+                    for (i=0; i<pcm_byte_per_frame*48*pcm_msec; i+=pcm_byte_per_frame>>1) {
+                        tmp1          = recvbuf[12+i];
+                        recvbuf[12+i] = recvbuf[13+i];
+                        recvbuf[13+i] = tmp1;
+                    }
                 }
-#else
-                for (i=(48*pcm_msec*2-1); i>=0; --i) {
-                    tmp1            = recvbuf[14+i*3];
-                    recvbuf[15+i*4] = recvbuf[12+i*3];
-                    recvbuf[14+i*4] = recvbuf[13+i*3];
-                    recvbuf[13+i*4] = tmp1;
-                    recvbuf[12+i*4] = 0;
-                }
-#endif
                 i2s_channel_write(tx_handle, recvbuf+12, pcm_byte_per_frame*48*pcm_msec, &bytes_written, 1000);
             } else {
                 ESP_LOGE(TAG, "multicast recvfrom failed: errno %d", errno);
@@ -269,15 +302,16 @@ static void mcast_example_task(void *pvParameters)
 
         }
 
+        /* Have to stop the channel before deleting it */
+        i2s_channel_disable(tx_handle);
+        /* If the handle is not needed any more, delete it to release the channel resources */
+        i2s_del_channel(tx_handle);
+
         ESP_LOGE(TAG, "Shutting down socket and restarting...");
         shutdown(sock, 0);
         close(sock);
-    }
 
-    /* Have to stop the channel before deleting it */
-    i2s_channel_disable(tx_handle);
-    /* If the handle is not needed any more, delete it to release the channel resources */
-    i2s_del_channel(tx_handle);
+    }
 
 }
 
